3cb43c8b4c47cabe0278f2b35885954743770f8b
Ned Ruggeri <ruggeri@self-loop.com>
2018-03-28 17:13:55 -0700

    keep writing ring buffer specs

--- lib/02_ring_buffer.rb
+++ lib/02_ring_buffer.rb
@@ -3,30 +3,45 @@ require_relative "00_static_array"
 class RingBuffer
   attr_reader :length
 
-  def initialize
+  def initialize(size)
+    self.store = StaticArray.new(size)
+    self.length = 0
+    self.capacity = size
+  end
+
+  # O(1)
+  def push(val)
+    check_can_insert!
+
+    self.length += 1
+    self.store[length - 1] = val
   end
 
   # O(1)
   def [](index)
-  end
-
-  # O(1)
-  def []=(index, val)
+    check_index!(index)
+    self.store[index]
   end
 
   # O(1)
   def pop
+    check_index!(0)
+
+    self.length -= 1
+    self.store[self.length]
   end
 
-  # O(1) ammortized
-  def push(val)
+  # O(1)
+  def []=(index, val)
+    check_index!(index)
+    self.store[self.length] = val
   end
 
   # O(1)
   def shift
   end
 
-  # O(1) ammortized
+  # O(1)
   def unshift(val)
   end
 
@@ -34,9 +49,11 @@ class RingBuffer
   attr_accessor :capacity, :start_idx, :store
   attr_writer :length
 
-  def check_index(index)
+  def check_can_insert!
+    raise 'ring buffer has no more space' if capacity == length
   end
 
-  def resize!
+  def check_index!(index)
+    raise "RingBuffer: index out of bounds" if index < 0 || length <= index
   end
 end
--- spec/02_ring_buffer_spec.rb
+++ spec/02_ring_buffer_spec.rb
@@ -2,24 +2,99 @@ require "02_ring_buffer"
 
 describe RingBuffer do
   it "starts out empty" do
-    arr = RingBuffer.new
+    arr = RingBuffer.new(8)
     expect(arr.length).to eq(0)
-    expect do
-      arr[0]
-    end.to raise_error("index out of bounds")
   end
 
-  it "pushes/pops items" do
-    arr = RingBuffer.new
+  it "starts out with a backing StaticArray with given capacity" do
+    arr = RingBuffer.new(8)
+
+    static_array = arr.send(:store)
+    expect(static_array).to be_a(StaticArray)
+    expect(static_array.send(:length)).to eq(8)
+    expect(arr.send(:capacity)).to eq(8)
+
+    arr = RingBuffer.new(16)
+    static_array = arr.send(:store)
+    expect(static_array.send(:length)).to eq(16)
+    expect(arr.send(:capacity)).to eq(16)
+  end
+
+  it "pushes items and allows you to get them using []" do
+    arr = RingBuffer.new(10)
+    10.times { |i| arr.push(i) }
+
+    expect(arr.length).to eq(10)
+    10.times { |i| expect(arr[i]) == i }
+  end
+
+  it "raises error if hits maximum capacity" do
+    arr = RingBuffer.new(10)
+    10.times { |i| arr.push(i) }
+
+    expect do
+      arr.push(11)
+    end.to raise_error('ring buffer has no more space')
+  end
+
+  it "cannot try to get with idx < 0 or length <= idx" do
+    arr = RingBuffer.new(10)
     5.times { |i| arr.push(i) }
 
-    expect(arr.length).to eq(5)
-    5.times { |i| expect(arr[i]) == i }
+    expect do
+      arr[5]
+    end.to raise_error("RingBuffer: index out of bounds")
+    expect do
+      arr[-1]
+    end.to raise_error("RingBuffer: index out of bounds")
+  end
 
-    4.downto(0) do |i|
+  it "pops items down zero length" do
+    arr = RingBuffer.new(10)
+    10.times { |i| arr.push(i) }
+
+    9.downto(0) do |i|
       expect(arr.pop).to eq(i)
     end
     expect(arr.length).to eq(0)
+  end
+
+  it "raises error if trying to pop when empty" do
+    arr = RingBuffer.new(10)
+    10.times { |i| arr.push(i) }
+    10.times { |i| arr.pop }
+
+    expect do
+      arr.pop
+    end.to raise_error("RingBuffer: index out of bounds")
+  end
+
+  it "sets items at an index" do
+    arr = RingBuffer.new(5)
+
+    5.times { arr.push(0) }
+    5.times { |i| arr[i] = i }
+    5.times { |i| expect(arr[i]).to eq(i) }
+  end
+
+  it "raises error when setting past end" do
+    arr = RingBuffer.new(5)
+
+    5.times { |i| arr.push(i) }
+
+    expect do
+      arr[5]
+    end.to raise_error("RingBuffer: index out of bounds")
+  end
+
+  it "raises error when setting before beginning" do
+    arr = RingBuffer.new(5)
+
+    5.times { |i| arr.push(i) }
+
+    expect do
+      arr[-1]
+    end.to raise_error("RingBuffer: index out of bounds")
   end
 
   it "unshifts/shifts items into array" do
