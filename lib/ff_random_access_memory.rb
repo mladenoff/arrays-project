299e7391f88936c843d8e63575a8ae8dbdc4d33e
Ned Ruggeri <ruggeri@self-loop.com>
2018-04-02 12:14:10 -0700

    rewrite RandomAccessMemory to be singleton

--- lib/ff_random_access_memory.rb
+++ lib/ff_random_access_memory.rb
@@ -1,21 +1,22 @@
+# This class simulates access to raw memory.
 class RandomAccessMemory
   # 8 bytes per word
   WORD_SIZE = 8
   START_ADDRESS = 800
-
-  def self.instance
-    @instance ||= RandomAccessMemory.new
-    @instance
-  end
 
   def initialize
     @current_address = START_ADDRESS
     @store = {}
   end
 
+  # Allocates `bytes` of memory, and returns a pointer to the start of
+  # the allocation.
   def malloc(bytes)
     if (bytes % WORD_SIZE) != 0
-      raise "this is a #{8*WORD_SIZE} bit machine; must allocate in multiples of #{WORD_SIZE} bytes"
+      raise (
+        "this is a #{8*WORD_SIZE} bit machine; " +
+        "must allocate in multiples of #{WORD_SIZE} bytes"
+      )
     end
 
     start_ptr = @current_address
@@ -26,6 +27,8 @@ class RandomAccessMemory
     start_ptr
   end
 
+  # Loads the value stored at the memory address specified by the
+  # pointer.
   def load(ptr)
     if !@store.has_key?(ptr)
       raise "Address #{ptr} was never allocated!"
@@ -34,11 +37,33 @@ class RandomAccessMemory
     @store[ptr]
   end
 
+  # Stores the value at the memory address specified by the pointer.
   def store(ptr, val)
     if !@store.has_key?(ptr)
       raise "Address #{ptr} was never allocated!"
     end
 
     @store[ptr] = val
+  end
+
+  # Singleton pattern implementation. Call methods directly on
+  # RandomAccessMemory class.
+  private_class_method :new
+
+  def self.instance
+    @instance ||= new
+    @instance
+  end
+
+  def self.malloc(bytes)
+    instance.malloc(bytes)
+  end
+
+  def self.load(ptr)
+    instance.load(ptr)
+  end
+
+  def self.store(ptr, val)
+    instance.store(ptr, val)
   end
 end
