5b9c85fc41b4ecb798b75151bab2becf6e33f61e
Ned Ruggeri <ruggeri@self-loop.com>
2018-04-02 11:33:31 -0700

    improve motivation for StaticArray class

--- lib/00_static_array.rb
+++ lib/00_static_array.rb
@@ -1,6 +1,20 @@
-# You must not use push or pop or shift or unshift for this class. Your
-# goal is to restrict a normal Array to a very restricted amount of
-# functionality.
+# The StaticArray class we build will be a restricted version of
+# the normal Ruby Array class.
+#
+# Our StaticArray will have `[]` and `[]=` methods, but no methods
+# that change the size of the array (like `push` or `pop`). It is a
+# "static" array because the size is static and fixed.
+#
+# You should back the store of the StaticArray with a normal Ruby
+# array instance. However, you must never push or pop into this. You
+# should make one Array object of the specified length once, and never
+# make additional Arrays.
+#
+# In the next phase, we will build back all the functionality of the
+# dynamic Ruby Array class (push, pop, et cetera) out of the restricted
+# functionality of the StaticArray. This emulates how Ruby builds
+# its Array class out of C arrays, which are very primitive and not
+# resizeable.
 class StaticArray
   def initialize(length)
   end
